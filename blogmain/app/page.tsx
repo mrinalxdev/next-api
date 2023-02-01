c7e329f1a97ef14938f0cd121b4c4d6db515b6c3
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-01 22:32:44 +0530

    Added Data

---
+++ blogmain/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return (
+        <div>
+            <h1 className="text-4xl">Welcome to my Blog</h1>
+            <p>Some Content</p>
+        </div>
+    )
+}
---
+++ blogmain/mrinalblog-nextjs/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'vcb7oq5w',
+    dataset: 'production'
+  }
+})
