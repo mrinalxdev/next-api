f6a63cfcdf2172aa0184b6c2e10a87dc3d6e9a56
bruh--Mrinal:))) <99001768+mrinalxdev@users.noreply.github.com>
2023-02-02 14:22:41 +0530

    Delete blogmain directory

--- blogmain/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage(){
-    return (
-        <div>
-            <h1 className="text-4xl">Welcome to my Blog</h1>
-            <p>Some Content</p>
-        </div>
-    )
-}
