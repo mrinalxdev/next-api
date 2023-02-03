2298d37e759469b47a819a27f7237564a6a3abae
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-03 22:05:40 +0530

    New Data Header.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ const Home: NextPage = () => {
           <div className="lg:col-span-4 col-span-1">
               <div className="lg:sticky relative top:8">
                 <PostWidget />
+                <Categories />
               </div>
           </div>
         </div>
