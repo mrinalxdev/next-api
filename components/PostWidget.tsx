0891d9e74cd7f78e102ab98a7c83987d057336a1
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-08 23:07:37 +0530

    Added Data

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -27,6 +27,29 @@ const PostWidget = ({ categories, slug }) => {
       <h3 className = "text-xl mb-8 font-semibold border-b pb-4">
         {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
+      {relatedPosts.map((post) => {
+        <div key={post.title} className = "flex items-center w-full mb-4">
+          <div className = "w-16 flex-none">
+            <img 
+              alt= {post.title}
+              height = "60px"
+              width = "60px"
+              className = "align-middle rounded-full"
+              src= {post.image.url}
+            />
+          </div>
+
+          <div className = "flex-grow ml-4">
+            <p className = "text-gray-500 font-xs">
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href= {`/post/${post.slug}`} key={post.title} className= "text-md ">
+              {post.title}
+            </Link>
+          </div>
+
+        </div>
+      })}
     </div>
   )
 }
