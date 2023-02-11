527962521375e11af5db9e66fd91946bff6f421a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-11 21:53:22 +0530

    Added Data

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { getPosts, getPostDetails } from '../ ../services'
-
-import { PostDetail, Categories, PsotWidget, Author, Comments, CommentsForm }
+import { PostDetail, Categories, PsotWidget, Author, Comments, CommentsForm } from '../ ../components'
 
 const PostDetails = () => {
   return (
-    <div>PostDetails</div>
+    <div className ="container mx-auto px-10 mb-8">
+      <div className = "grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className = "col-span-1 lg:col-sapn-8">
+          <PostDetail />
+          <Author />
+          <CommentsForm />
+          <Commetns />
+        </div>
+        <div className = "col-span-1 lg:col-sapn-4">
+          <div className = "relative lg:sticky top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
