2bb44c88bf3a1100c15db3fc4221e6e99614eaf9
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-15 23:12:14 +0530

    Done with the Navigation Panel

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './Navigation.module.css'
+
 
 const Navigation = () => {
+
+  const logoStyle = {
+    color : '#fff',
+    textDecoration: 'none',
+    fontSize : '25px',
+    fontWeight : '700',
+    display : 'flex', 
+    alignItems : 'center'
+  }
+
   return (
-    <h1>This is Nav </h1>
+    <nav className = {`${styles.navbar} container`} >
+      <Link style={logoStyle} to = "/">
+        <span>💻 DevMeets</span>
+      </Link>
+    </nav>
   )
 }
 
