5c0f2574ad3c5329ae1051f1265ad5af41a3a20a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-18 19:17:58 +0530

    added Data

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <h1>This is Home</h1>
+    <div className = {styles.card}>
+
+      <div className = {styles.headWrapper}>
+        <span>✋</span>
+        <h1>Welcome to DevMeets !</h1>
+      </div>
+
+      <p>This is the DevMeet's first prototype . While the 1st version is under development 🔥. Try its beta version and be the part of my project 😃 </p>
+
+      <div>
+        <button> 🤗 Join the Beta</button>
+        <span>Have an Invite Text?</span>
+        <Link to= "/login">Sign In</Link>
+      </div>
+    </div>
   )
 }
 
